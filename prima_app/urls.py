f0a364e826ea9f08d3339909b32b1dd7b1ac6c0c
FrancescoLatella <115148094+FrancescoLatella@users.noreply.github.com>
2022-10-06 13:17:03 +0200

    aggiunte variabili

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from prima_app.views import homepage
+from prima_app.views import homepage, variabili
 app_name="prima_app"
 urlpatterns=[
-    path('', homepage,name='homepage'),
-]
+    path('homepage', homepage,name='homepage'),
+    path('variabili', variabili,name='variabili'),
+]   
