97731f1af847e475e00e2ad09fe8604ff2200540
FrancescoLatella <115148094+FrancescoLatella@users.noreply.github.com>
2022-10-06 12:39:56 +0200

    Update settings.py

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k5)3smbt_88im4meo80+h%k$ojkyndx$9zztlj9l0s+h5m(-0n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
